8ae8936c4e7025ea91535b9144b324b383aebf8c
Karina Dalca <karinadalca@gmail.com>
2017-05-05 02:31:21 -0700

    two test added to uniq.test.js

--- test/uniq.test.js
+++ test/uniq.test.js
@@ -5,4 +5,14 @@ describe('uniq()', () => {
     const nums = [2, 4, 5, 5, 7, 4, 10, 2];
     expect(_.uniq(nums)).toEqual([2, 4, 5, 7, 10]);
   });
+
+  it('de-dups an array of strings', () => {
+    const str = ['foo', 'bar', 'foo', 'baz', 'hat', 'bar'];
+    expect(_.uniq(str)).toEqual(['foo', 'bar', 'baz', 'hat']);
+  });
+
+  it('de-dups an array of empty arrays', () => {
+    const arr = [[],[],[],[],[]];
+    expect(_.uniq(arr)).toEqual([[]]);
+  });
 });
