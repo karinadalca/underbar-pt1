c25fe05f35a96ac10f19343cfdc1a5e791c3806b
Karina Dalca <karinadalca@gmail.com>
2017-05-05 00:46:14 -0700

    added two tests to last.test.js

--- test/last.test.js
+++ test/last.test.js
@@ -12,4 +12,12 @@ describe('last()', () => {
   it('returns the whole array if you ask for more elements than it has', () => {
     expect(_.last(['a', 'b', 'c'], 5)).toEqual(['a', 'b', 'c']);
   });
+
+  it('returns empty array if you ask for ZERO elements', () => {
+    expect(_.last(['a', 'b', 'c'], 0)).toEqual([]);
+  });
+
+  it('returns empty array if you ask for ZERO elements', () => {
+    expect(_.last(['a', 'b', 'c'], -1)).toEqual([]);
+  });
 });
