fe39b8783eb7105aabfe4899fdcb5596dc138f26
Karina Dalca <karinadalca@gmail.com>
2017-05-05 00:40:53 -0700

    added zero element test to first.test.js

--- test/first.test.js
+++ test/first.test.js
@@ -12,4 +12,8 @@ describe('first()', () => {
   it('returns the whole array if you ask for more elements than it has', () => {
     expect(_.first(['a', 'b', 'c'], 5)).toEqual(['a', 'b', 'c']);
   });
+
+  it('returns empty array if you ask for ZERO elements', () => {
+    expect(_.first(['a', 'b', 'c'], 0)).toEqual([]);
+  });
 });
